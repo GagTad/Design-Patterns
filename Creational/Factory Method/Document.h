e0436631e45dec2ddeb7e75abeb8ac3882e02eb0
GagTad <136594120+GagTad@users.noreply.github.com>
2025-10-20 23:53:01 +0400

    Remove file header comment from Document.h
    Remove file header comment from Document.h

---
+++ Creational/Factory Method/Document.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+// 1. Abstract Product (Product)
+class Document {
+public:
+    virtual ~Document() = default;
+    virtual void open() const = 0;
+    virtual void save() const = 0;
+    virtual void close() const = 0;
+};
+
+// 2. Concrete Product 1
+class TextDocument : public Document {
+public:
+    void open() const override {
+        std::cout << "Opening Text Document..." << std::endl;
+    }
+    void save() const override {
+        std::cout << "Saving Text Document..." << std::endl;
+    }
+    void close() const override {
+        std::cout << "Closing Text Document..." << std::endl;
+    }
+};
+
+// 3. Concrete Product 2
+class SpreadsheetDocument : public Document {
+public:
+    void open() const override {
+        std::cout << "Opening Spreadsheet Document..." << std::endl;
+    }
+    void save() const override {
+        std::cout << "Saving Spreadsheet Document..." << std::endl;
+    }
+    void close() const override {
+        std::cout << "Closing Spreadsheet Document..." << std::endl;
+    }
+};
