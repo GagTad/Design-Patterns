32d678b65e778a2eb2d844a1aad4b0e07ef6241f
GagTad <136594120+GagTad@users.noreply.github.com>
2025-10-21 07:13:11 +0400

    Update computerBuilder.h
    Added ComputerBuilder class with methods to set computer components.

--- Creational/Builder/computerBuilder.h
+++ Creational/Builder/computerBuilder.h
@@ -1,1 +1,21 @@
 #pragma once
+
+#include <string>
+#include <memory> 
+ 
+class Computer;
+
+class ComputerBuilder {
+private:
+    std::unique_ptr<Computer> m_computer;
+
+public:
+    ComputerBuilder();
+ 
+    ComputerBuilder& setCPU(const std::string& cpu);
+    ComputerBuilder& setRAM(const std::string& ram);
+    ComputerBuilder& setStorage(const std::string& storage);
+    ComputerBuilder& setGPU(const std::string& gpu);
+ 
+    std::unique_ptr<Computer> build();
+};
