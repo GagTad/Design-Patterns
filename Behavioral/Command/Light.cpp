efa9e9d119241ea01647e8cfe2d0b85456a30e18
GagTad <136594120+GagTad@users.noreply.github.com>
2025-12-12 20:48:33 +0400

    Implement Light class with turnOn and turnOff methods

---
+++ Behavioral/Command/Light.cpp
@@ -0,0 +1,10 @@
+#include "Light.h"
+#include <iostream>
+
+void Light::turnOn() {
+    std::cout << "Light is ON" << std::endl;
+}
+
+void Light::turnOff() {
+    std::cout << "Light is OFF" << std::endl;
+}
